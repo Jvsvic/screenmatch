69a46b10eec2a50a828edb9717cd3a091c06e3ab
João Vitor de Oliveira <155332038+Jvsvic@users.noreply.github.com>
2026-09-25 18:18:52 +0000

    sistema de compras com ordenação por valor

--- src/br/com/alura/screenmatch/exercicios/Compras.java
+++ src/br/com/alura/screenmatch/exercicios/Compras.java
@@ -1,5 +1,34 @@
 package br.com.alura.screenmatch.exercicios;
 
-public class Compras {
-    
+import java.util.Comparator;
+
+public class Compras implements Comparable<Compras>{
+    private double valor;
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public double getValor() {
+        return valor;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    public Compras(double valor ,String descricao){
+        this.valor = valor;
+        this.descricao = descricao;
+
+    }
+    @Override
+    public String toString() {
+        return getDescricao() + " - " + getValor();
+    }
+    @Override
+    public int compareTo(Compras outrasCompras) {
+        return Double.compare(this.getValor(), outrasCompras.getValor());
+    }
 }
--- src/br/com/alura/screenmatch/principal/PrincipalCompras.java
+++ src/br/com/alura/screenmatch/principal/PrincipalCompras.java
@@ -1,12 +1,19 @@
 package br.com.alura.screenmatch.principal;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+
+import br.com.alura.screenmatch.exercicios.Compras;
+
 
 public class PrincipalCompras {
     public static void main(String[] args) {
         
 
         Scanner leitura = new Scanner(System.in);
+        List<Compras> descricaoDasCompras = new ArrayList<>();
         
         System.out.println("Digite o limite do cartão: ");
         double limite = leitura.nextDouble();
@@ -18,24 +25,47 @@ public class PrincipalCompras {
         System.out.println("Digite a descrição da compra: ");
         String descricao = leitura.nextLine();
 
+
+
         System.out.println("Digite o valor da compra: ");
         double valor = leitura.nextDouble();
         leitura.nextLine();
+        
 
         if (valor <= limite) {
             System.out.println("Compra realizada. ");
             limite = limite - valor;
+            Compras compra = new Compras(valor, descricao);
+            descricaoDasCompras.add(compra);
+            Collections.sort(descricaoDasCompras);
+            System.out.println(descricaoDasCompras);
             System.out.println("Limite atual: " + limite);
         } else {
             System.out.println("Saldo insuficiente! ");
+            System.out.println("Os itens comprados foram: ");
+            for (Compras todasAsDescricoes : descricaoDasCompras) {
+            System.out.println(todasAsDescricoes);
         }
-        
+        }
+            
         System.out.println("Digite 1 para continuar ou 0 para parar.");
         opcao = leitura.nextInt();
         leitura.nextLine();
-        }
+
+
+
         
+        }
+        System.out.println("Seu limite terminou em: " + limite);
+        System.out.println("Os itens comprados foram: ");
+        Collections.sort(descricaoDasCompras);
+        for (Compras todasAsDescricoes : descricaoDasCompras) {
+            
+            System.out.println(todasAsDescricoes);
+        }
+        leitura.close();
         
         
     }
 }
+
